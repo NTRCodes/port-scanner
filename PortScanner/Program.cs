7e74eecd186db80e5f6153e8e76b6a3dc63cb363
nr44c <nigel.ross85@gmail.com>
2026-06-02 13:43:50 -0700

    Add project files.

---
+++ PortScanner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Sockets;
+
+string target = "127.0.0.1";
+int port = 80;
+
+using var client = new TcpClient();
+var result = client.BeginConnect(target, port, null, null);
+bool success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+
+if (success)
+    Console.WriteLine($"Port {port} is OPEN");
+else
+    Console.WriteLine($"Port {port} is CLOSED");
